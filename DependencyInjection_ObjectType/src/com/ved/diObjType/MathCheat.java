609df09c1bc5389f4a4c9c8b410047bc66592976
vedmk32 <vedmk32@gmail.com>
2021-04-16 21:39:04 +0530

    class modified!!

--- DependencyInjection_ObjectType/src/com/ved/diObjType/MathCheat.java
+++ DependencyInjection_ObjectType/src/com/ved/diObjType/MathCheat.java
@@ -3,6 +3,6 @@ package com.ved.diObjType;
 public class MathCheat {
 
 	public void mathCheatMethod() {
-		System.out.println("math cheating started");
+		System.out.println("math cheating started!!");
 	}
 }
