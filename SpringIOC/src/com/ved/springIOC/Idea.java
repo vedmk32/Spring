915a2dc2804c1ed8a99bb47e65e3fce6db3c5421
vedmk32 <vedmk32@gmail.com>
2021-04-21 14:51:38 +0530

    added class

---
+++ SpringIOC/src/com/ved/springIOC/Idea.java
@@ -0,0 +1,20 @@
+package com.ved.springIOC;
+
+public class Idea implements Sim{
+
+	 
+	
+	@Override
+	public void calling() {
+		System.out.println("calling with Idea!!");
+		
+	}
+
+	@Override
+	public void data() {
+		System.out.println("using Idea data");
+		
+	}
+	
+ 
+}
